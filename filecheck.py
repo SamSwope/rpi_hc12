a482b870590504763919124026cfde72dcc4481c
Samuel Swope <samc.swope@gmail.com>
2021-06-02 12:19:08 -0400

    New branch to create SQLite3 interaction instead of using .txt

---
+++ filecheck.py
@@ -0,0 +1,12 @@
+import os
+
+# Function checks to see if file x is in directory d
+# x is a string
+# Returns True if x is in d, else False
+def checkforfile(x, d):
+    files = os.listdir(d)
+    for f in files:
+	if (f == x):
+	    return True
+    
+    return False
