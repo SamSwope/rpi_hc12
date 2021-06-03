1397209602cf2b71cf66a60e3b438b812beaf067
Samuel Swope <samc.swope@gmail.com>
2021-06-03 15:08:34 -0400

    Start of webserver using Flask only so far

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/device_<name>.html')
+def device(name):
+    return render_template("device.html",device=name)
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
