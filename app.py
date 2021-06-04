0cd365cfe03aa7502a77b4967d0f8d6bada599d3
Samuel Swope <samc.swope@gmail.com>
2021-06-04 16:27:25 -0400

    -app.py does not have previous Chart.js handling
    -app.py now has more imports, so it can handle matplotlib and flask
    
    -app.py now accesses current databases for devices
    
    -device.hmtl now has dynamic access to device page
    
    -device.html shows a plot for d1 for device 3W

--- app.py
+++ app.py
@@ -1,14 +1,77 @@
-from flask import Flask, render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+import os
+from flask import Flask, render_template, send_file, make_response, request, abort, url_for
+import sqlite3
+
+
+def getdata(c):
+	c.execute("SELECT * FROM data")
+	data = c.fetchall()
+	date = []
+	d1 = []
+	d2 = []
+	d3 = []
+	d4 = []
+	d5 = []
+	for row in data:
+		date.append(row[0] + " " + row[1])
+		d1.append(row[2])
+		d2.append(row[3])
+		d3.append(row[4])
+		d4.append(row[5])
+		d5.append(row[6])
+	return date, d1, d2, d3, d4, d5
+
 
 app = Flask(__name__)
 
+# Default Entry (index.html)
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# Dynamic access to each device 
 @app.route('/device_<name>.html')
 def device(name):
-    return render_template("device.html",device=name)
+	return render_template("device.html",device=name)
+
+# Plotting Data 1 for Device 
+@app.route('/plot/<name>_d<num>')
+def plot_data(name,num):
+	cdir = os.getcwd()
+	try:
+		f = cdir + "/%s.db" % name
+	except:
+		abort(404)
+	con = sqlite3.connect(f)
+	c = con.cursor()
+	date, d1, d2, d3, d4, d5 = getdata(c)
+	if num == 1:
+		ys = d1
+	elif num == 2:
+		ys = d2
+	elif num == 3:
+		ys = d3
+	elif num == 4:
+		ys = d4
+	else:
+		ys = d5
+
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("Data 1 for %s" % name)
+	axis.set_xlabel("Date")
+	axis.grid(True)
+	xs = range(len(date))
+	axis.plot(xs,ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
